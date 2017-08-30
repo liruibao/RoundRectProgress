cc644e30c2fb1a32da59613334bc4d1656ecc74e
liruibao <liruibao@baidu.com>
2017-08-30 11:29:44 +0800

    小球 气泡碰撞算法

---
+++ app/src/main/java/com/example/liruibao/exprogress/MenuActivity.java
@@ -0,0 +1,19 @@
+package com.example.liruibao.exprogress;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+
+
+    private Button mExProgressBtn;
+    private Button mBallBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
