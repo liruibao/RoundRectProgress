35adec21022d6ef447309fc7af522d60197cd908
liruibao <liruibao@baidu.com>
2017-09-04 19:26:31 +0800

    添加了水纹的view效果

--- app/src/main/java/com/example/liruibao/exprogress/MainActivity.java
+++ app/src/main/java/com/example/liruibao/exprogress/MainActivity.java
@@ -6,6 +6,8 @@ import android.graphics.Path;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.liruibao.exprogress.view.ExProgressView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
